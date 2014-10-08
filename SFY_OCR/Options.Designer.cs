3acaa6470353090cda26a43387125e31d84db0a1
ningboliuwei <liuwei@nbu.edu.cn>
2014-10-08 14:15:13 +0800

    选项窗体轻微 UI 改动

--- SFY_OCR/Options.Designer.cs
+++ SFY_OCR/Options.Designer.cs
@@ -30,8 +30,6 @@
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Options));
 			this.tabControl1 = new System.Windows.Forms.TabControl();
-			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-			this.fbdChooseDir = new System.Windows.Forms.FolderBrowserDialog();
 			this.basicOptions = new System.Windows.Forms.TabPage();
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
@@ -44,8 +42,10 @@
 			this.btnOK = new System.Windows.Forms.Button();
 			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
 			this.txtTesseractOcrDir = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
 			this.btnChooseTesseractOcrDir = new System.Windows.Forms.Button();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.fbdChooseDir = new System.Windows.Forms.FolderBrowserDialog();
+			this.label1 = new System.Windows.Forms.Label();
 			this.tabControl1.SuspendLayout();
 			this.basicOptions.SuspendLayout();
 			this.tableLayoutPanel1.SuspendLayout();
@@ -61,7 +61,7 @@
 			this.tabControl1.Location = new System.Drawing.Point(0, 0);
 			this.tabControl1.Name = "tabControl1";
 			this.tabControl1.SelectedIndex = 0;
-			this.tabControl1.Size = new System.Drawing.Size(484, 161);
+			this.tabControl1.Size = new System.Drawing.Size(504, 161);
 			this.tabControl1.TabIndex = 0;
 			// 
 			// basicOptions
@@ -70,7 +70,7 @@
 			this.basicOptions.Location = new System.Drawing.Point(4, 26);
 			this.basicOptions.Name = "basicOptions";
 			this.basicOptions.Padding = new System.Windows.Forms.Padding(3);
-			this.basicOptions.Size = new System.Drawing.Size(476, 131);
+			this.basicOptions.Size = new System.Drawing.Size(496, 131);
 			this.basicOptions.TabIndex = 0;
 			this.basicOptions.Text = "基本设置";
 			this.basicOptions.UseVisualStyleBackColor = true;
@@ -89,7 +89,7 @@
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(470, 125);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(490, 125);
 			this.tableLayoutPanel1.TabIndex = 1;
 			// 
 			// tableLayoutPanel4
@@ -106,14 +106,14 @@
 			this.tableLayoutPanel4.Name = "tableLayoutPanel4";
 			this.tableLayoutPanel4.RowCount = 1;
 			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel4.Size = new System.Drawing.Size(464, 29);
+			this.tableLayoutPanel4.Size = new System.Drawing.Size(484, 29);
 			this.tableLayoutPanel4.TabIndex = 21;
 			// 
 			// txtOutputDir
 			// 
 			this.txtOutputDir.BackColor = System.Drawing.SystemColors.Window;
 			this.txtOutputDir.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.txtOutputDir.Location = new System.Drawing.Point(167, 3);
+			this.txtOutputDir.Location = new System.Drawing.Point(187, 3);
 			this.txtOutputDir.Name = "txtOutputDir";
 			this.txtOutputDir.ReadOnly = true;
 			this.txtOutputDir.Size = new System.Drawing.Size(244, 23);
@@ -125,15 +125,15 @@
 			this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.label2.Location = new System.Drawing.Point(3, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(158, 29);
+			this.label2.Size = new System.Drawing.Size(178, 29);
 			this.label2.TabIndex = 1;
 			this.label2.Text = "识别结果输出文件夹：";
-			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
 			// 
 			// btnChooseOutputDir
 			// 
 			this.btnChooseOutputDir.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.btnChooseOutputDir.Location = new System.Drawing.Point(417, 3);
+			this.btnChooseOutputDir.Location = new System.Drawing.Point(437, 3);
 			this.btnChooseOutputDir.Name = "btnChooseOutputDir";
 			this.btnChooseOutputDir.Size = new System.Drawing.Size(44, 23);
 			this.btnChooseOutputDir.TabIndex = 4;
@@ -155,15 +155,15 @@
 			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
 			this.tableLayoutPanel2.RowCount = 1;
 			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel2.Size = new System.Drawing.Size(464, 49);
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(484, 49);
 			this.tableLayoutPanel2.TabIndex = 19;
 			// 
 			// btnDefault
 			// 
 			this.btnDefault.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.btnDefault.Location = new System.Drawing.Point(311, 3);
+			this.btnDefault.Location = new System.Drawing.Point(325, 3);
 			this.btnDefault.Name = "btnDefault";
-			this.btnDefault.Size = new System.Drawing.Size(150, 43);
+			this.btnDefault.Size = new System.Drawing.Size(156, 43);
 			this.btnDefault.TabIndex = 3;
 			this.btnDefault.Text = "默认(&D)";
 			this.btnDefault.UseVisualStyleBackColor = true;
@@ -172,9 +172,9 @@
 			// btnCancel
 			// 
 			this.btnCancel.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.btnCancel.Location = new System.Drawing.Point(157, 3);
+			this.btnCancel.Location = new System.Drawing.Point(164, 3);
 			this.btnCancel.Name = "btnCancel";
-			this.btnCancel.Size = new System.Drawing.Size(148, 43);
+			this.btnCancel.Size = new System.Drawing.Size(155, 43);
 			this.btnCancel.TabIndex = 1;
 			this.btnCancel.Text = "取消(&C)";
 			this.btnCancel.UseVisualStyleBackColor = true;
@@ -185,7 +185,7 @@
 			this.btnOK.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.btnOK.Location = new System.Drawing.Point(3, 3);
 			this.btnOK.Name = "btnOK";
-			this.btnOK.Size = new System.Drawing.Size(148, 43);
+			this.btnOK.Size = new System.Drawing.Size(155, 43);
 			this.btnOK.TabIndex = 0;
 			this.btnOK.Text = "确定(&O)";
 			this.btnOK.UseVisualStyleBackColor = true;
@@ -197,42 +197,31 @@
 			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
 			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 250F));
 			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-			this.tableLayoutPanel3.Controls.Add(this.txtTesseractOcrDir, 1, 0);
 			this.tableLayoutPanel3.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel3.Controls.Add(this.txtTesseractOcrDir, 1, 0);
 			this.tableLayoutPanel3.Controls.Add(this.btnChooseTesseractOcrDir, 2, 0);
 			this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
 			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
 			this.tableLayoutPanel3.RowCount = 1;
 			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel3.Size = new System.Drawing.Size(464, 29);
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(484, 29);
 			this.tableLayoutPanel3.TabIndex = 20;
 			// 
 			// txtTesseractOcrDir
 			// 
 			this.txtTesseractOcrDir.BackColor = System.Drawing.SystemColors.Window;
 			this.txtTesseractOcrDir.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.txtTesseractOcrDir.Location = new System.Drawing.Point(167, 3);
+			this.txtTesseractOcrDir.Location = new System.Drawing.Point(187, 3);
 			this.txtTesseractOcrDir.Name = "txtTesseractOcrDir";
 			this.txtTesseractOcrDir.ReadOnly = true;
 			this.txtTesseractOcrDir.Size = new System.Drawing.Size(244, 23);
 			this.txtTesseractOcrDir.TabIndex = 4;
 			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.label1.Location = new System.Drawing.Point(3, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(158, 29);
-			this.label1.TabIndex = 3;
-			this.label1.Text = "tesseract.exe 所在文件夹：";
-			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
 			// btnChooseTesseractOcrDir
 			// 
 			this.btnChooseTesseractOcrDir.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.btnChooseTesseractOcrDir.Location = new System.Drawing.Point(417, 3);
+			this.btnChooseTesseractOcrDir.Location = new System.Drawing.Point(437, 3);
 			this.btnChooseTesseractOcrDir.Name = "btnChooseTesseractOcrDir";
 			this.btnChooseTesseractOcrDir.Size = new System.Drawing.Size(44, 23);
 			this.btnChooseTesseractOcrDir.TabIndex = 5;
@@ -240,11 +229,22 @@
 			this.btnChooseTesseractOcrDir.UseVisualStyleBackColor = true;
 			this.btnChooseTesseractOcrDir.Click += new System.EventHandler(this.btnChooseTesseractOcrDir_Click);
 			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Location = new System.Drawing.Point(3, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(178, 29);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "tesseract.exe 所在文件夹：";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
 			// Options
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(484, 161);
+			this.ClientSize = new System.Drawing.Size(504, 161);
 			this.Controls.Add(this.tabControl1);
 			this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -284,10 +284,10 @@
 		private System.Windows.Forms.Button btnOK;
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
 		private System.Windows.Forms.TextBox txtTesseractOcrDir;
-		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button btnChooseTesseractOcrDir;
 		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
 		private System.Windows.Forms.FolderBrowserDialog fbdChooseDir;
+		private System.Windows.Forms.Label label1;
 
 	}
 }
